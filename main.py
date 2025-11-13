33032688d7811fe3f5b726aec916be3b20e6cecf
Luckey Sherpa <luckeysherpa07@gmail.com>
2025-11-13 11:30:31 +0100

    added calibration info file to the project

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from camera_feature import display_depth_1_live
 from camera_feature import display_event_live
 from camera_feature import display_depth_1_event_live
+from camera_feature import display_calibration_info
 
 def main():
     options = {
         "1": ("Display Depth 1 Live", display_depth_1_live.run),
         "2": ("Display Event Live", display_event_live.run),
         "3": ("Display Depth 1 and Event Live", display_depth_1_event_live.run),
+        "4": ("Display Calibration Info", display_calibration_info.run),
     }
 
     while True:
