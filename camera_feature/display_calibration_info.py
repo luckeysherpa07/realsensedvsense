25b749b9889fd728c2dd0e43f4ced33719e622f7
Luckey Sherpa <luckeysherpa07@gmail.com>
2025-11-13 12:23:01 +0100

    added intrinsic paramter of the depth camera

--- camera_feature/display_calibration_info.py
+++ camera_feature/display_calibration_info.py
@@ -1,35 +1,28 @@
 import pyrealsense2 as rs
-import numpy as np
-import cv2
 
 def run():
-    pipe = rs.pipeline()
-    cfg = rs.config()
+    pipeline = rs.pipeline()
+    pipeline.start()
 
-    cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-    cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    # Get the active profile and depth stream profile
+    profile = pipeline.get_active_profile()
+    depth_stream = profile.get_stream(rs.stream.depth)
+    depth_video_profile = rs.video_stream_profile(depth_stream)
 
-    pipe.start(cfg)
+    # Get intrinsics object
+    intrinsics = depth_video_profile.get_intrinsics()
 
-    print('Calibration Info')
+    fx = intrinsics.fx  # focal length x
+    fy = intrinsics.fy  # focal length y
+    cx = intrinsics.ppx # principal point x
+    cy = intrinsics.ppy # principal point y
+    width = intrinsics.width
+    height = intrinsics.height
+    distortion = intrinsics.coeffs # distortion coefficients
 
-    while True: 
-        frame = pipe.wait_for_frames()
-        depth_frame = frame.get_depth_frame()
-        color_frame = frame.get_color_frame()
-
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        depth_cm = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.5), cv2.COLORMAP_JET)
-
-        cv2.imshow('rgb', color_image)
-        cv2.imshow('depth', depth_cm)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    pipe.stop()
-    cv2.destroyAllWindows()
+    print("INTRINSIC PARAMETER of Depth Camera")
+    print(f"fx={fx}\nfy={fy}\ncx={cx}\ncy={cy}\nwidth={width}\nheight={height}")
+    print(f"Distortion coefficients: {distortion}")
 
 if __name__ == "__main__":
     run()
